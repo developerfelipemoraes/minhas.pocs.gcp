b917a16ce290b8ae4d7249bab75c105ed4c9dbe7
developerfelipemoraes <developer-felipe@outlook.com>
2026-02-10 10:17:49 -0300

    clean repo

--- v2/Services/IGcsResumableSigner.cs
+++
@@ -1,7 +0,0 @@
-namespace Aurovel.GcsUpload.Services;
-
-public interface IGcsResumableSigner
-{
-    /// Cria URL V4 assinada para iniciar sessão resumível (POST + x-goog-resumable: start).
-    string CreateResumableStartUrl(string objectName, TimeSpan lifetime);
-}
